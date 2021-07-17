9e1dc6e73c3423d2ee4b189f8d839dbc1cf66e97
Arup paul <aruppaulctg@gmail.com>
2021-07-17 15:27:05 +0600

    admin controller create and login process

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Session;
+
+
+class AdminController extends Controller
+{
+    //admin login
+    public function adminLogin(){
+        return view('admin.login');
+    }
+
+    public function adminLoginProcess(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+        else{
+            if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password])){
+                return redirect()->route('admin.dashboard');
+            }else{
+                Session::flash('error_message','Invalid Email Or Password');
+                return redirect('/admin/login');
+            }
+
+        }
+
+    }
+
+    public function dashboard(){
+        return view('admin.dashboard');
+    }
+}
